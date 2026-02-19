7fa6af95fc75a34ceba38bf13cb428085a398925
Ido Sofi <ido@cvedia.com>
2026-02-19 20:04:31 +0200

    noice

---
+++ src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import styles from "./Main.module.scss";
+
+function Main() {
+
+    return (
+        <div className={styles.container}>
+            Test
+        </div>
+    );
+}
+export default Main;
